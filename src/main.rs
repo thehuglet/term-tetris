25a6ee1a7bd8da557822d879643fc89a024e0c34
Amy Opas <amy.opas@proton.me>
2026-02-03 21:48:43 +0100

    Added tetromino bitmasks definitions

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,121 @@
 mod tetromino;
 
 use germterm::{
-    color::Color,
+    color::{Color, lerp},
     crossterm::event::{Event, KeyCode, KeyEvent},
-    draw::{Layer, fill_screen},
+    draw::{Layer, draw_rect, draw_twoxel},
     engine::{Engine, end_frame, exit_cleanup, init, start_frame},
-    fps_counter::draw_fps_counter,
     input::poll_input,
 };
 use std::io;
 
+use crate::tetromino::{
+    Tetromino, rotate_90_clockwise, rotate_90_counter_clockwise, tetromino_bitmask,
+};
+
+struct TetrominoState {
+    bitmask: u16,
+    x: i16,
+    y: i16,
+}
+
 fn main() -> io::Result<()> {
-    let mut engine = Engine::new(40, 20).limit_fps(60);
+    let mut engine = Engine::new(40, 20).title("term-tetris").limit_fps(240);
     let mut layer = Layer::new(&mut engine, 0);
 
     init(&mut engine)?;
 
-    'update_loop: loop {
-        start_frame(&mut engine);
+    let fall_speed: f32 = 1.0;
+    let mut fall_timer: f32 = 0.0;
+    let mut controlled_tetromino = TetrominoState {
+        bitmask: tetromino_bitmask(&Tetromino::I),
+        x: 4,
+        y: 4,
+    };
 
+    'update_loop: loop {
         for event in poll_input() {
-            if let Event::Key(KeyEvent {
-                code: KeyCode::Char('q'),
-                ..
-            }) = event
-            {
-                break 'update_loop;
+            match event {
+                Event::Key(KeyEvent {
+                    code: KeyCode::Char('q'),
+                    ..
+                }) => break 'update_loop,
+                Event::Key(KeyEvent {
+                    code: KeyCode::Char('e'),
+                    ..
+                }) => {
+                    controlled_tetromino.bitmask =
+                        rotate_90_counter_clockwise(controlled_tetromino.bitmask)
+                }
+                Event::Key(KeyEvent {
+                    code: KeyCode::Char('r'),
+                    ..
+                }) => {
+                    controlled_tetromino.bitmask = rotate_90_clockwise(controlled_tetromino.bitmask)
+                }
+                Event::Key(KeyEvent {
+                    code: KeyCode::Left,
+                    ..
+                }) => controlled_tetromino.x -= 1,
+                Event::Key(KeyEvent {
+                    code: KeyCode::Right,
+                    ..
+                }) => controlled_tetromino.x += 1,
+                _ => {}
             }
         }
+        start_frame(&mut engine);
+        let step_time: f32 = 1.0 / fall_speed;
+        fall_timer += engine.delta_time;
 
-        // Draw contents
-        fill_screen(&mut layer, Color::BLACK);
+        if fall_timer >= step_time {
+            fall_timer -= step_time;
+            controlled_tetromino.y += 1;
+        }
+
+        draw_tetromino(
+            &mut layer,
+            &controlled_tetromino,
+            controlled_tetromino.x,
+            controlled_tetromino.y,
+            Color::TEAL,
+        );
+
+        draw_rect(&mut layer, 10, 10, 8, 1, Color::RED);
+        // fill_screen(&mut layer, Color::BLACK);
         // draw_text(&mut layer, 14, 9, "Hello world!");
-        draw_fps_counter(&mut layer, 0, 0);
+        // draw_fps_counter(&mut layer, 0, 0);
 
         end_frame(&mut engine)?;
     }
 
     exit_cleanup(&mut engine)?;
     Ok(())
+}
+
+fn draw_tetromino(
+    layer: &mut Layer,
+    tetromino_state: &TetrominoState,
+    pos_x: i16,
+    pos_y: i16,
+    color: Color,
+) {
+    for y in 0..4 {
+        for x in 0..4 {
+            let pixel_x = pos_x + x;
+            let pixel_y = pos_y + y;
+
+            // Checkerboard dimming to differentiate each pixel a little bit
+            let color = if (x + y) % 2 == 0 {
+                lerp(color, Color::BLACK.with_alpha(color.a()), 0.15)
+            } else {
+                color
+            };
+
+            let bit = 1 << (15 - (y * 4 + x));
+            if tetromino_state.bitmask & bit != 0 {
+                draw_twoxel(layer, pixel_x as f32, pixel_y as f32 * 0.5, color);
+            }
+        }
+    }
 }
--- src/tetromino.rs
+++ src/tetromino.rs
@@ -1,1 +1,128 @@
-pub enum Tetromino {}
+const fn build_tetromino_bitmask(rows: [&str; 4]) -> u16 {
+    let mut bits = 0u16;
+    let mut y = 0;
+    while y < 4 {
+        let bytes = rows[y].as_bytes();
+        let mut x = 0;
+        while x < 4 {
+            if bytes[x] == b'o' {
+                bits |= 1 << (15 - (y * 4 + x));
+            }
+            x += 1;
+        }
+        y += 1;
+    }
+    bits
+}
+
+#[rustfmt::skip]
+pub const I_TETROMINO: u16 = build_tetromino_bitmask([
+    "----",
+    "oooo",
+    "----",
+    "----",
+]);
+
+#[rustfmt::skip]
+pub const O_TETROMINO: u16 = build_tetromino_bitmask([
+    "oo--",
+    "oo--",
+    "----",
+    "----",
+]);
+
+#[rustfmt::skip]
+pub const T_TETROMINO: u16 = build_tetromino_bitmask([
+    "ooo-",
+    "-o--",
+    "----",
+    "----",
+]);
+
+#[rustfmt::skip]
+pub const J_TETROMINO: u16 = build_tetromino_bitmask([
+    "-o--",
+    "-o--",
+    "oo--",
+    "----",
+]);
+
+#[rustfmt::skip]
+pub const L_TETROMINO: u16 = build_tetromino_bitmask([
+    "o---",
+    "o---",
+    "oo--",
+    "----",
+]);
+
+#[rustfmt::skip]
+pub const S_TETROMINO: u16 = build_tetromino_bitmask([
+    "-oo-",
+    "oo--",
+    "----",
+    "----",
+]);
+
+#[rustfmt::skip]
+pub const Z_TETROMINO: u16 = build_tetromino_bitmask([
+    "oo--",
+    "-oo-",
+    "----",
+    "----",
+]);
+
+pub enum Tetromino {
+    I,
+    O,
+    T,
+    J,
+    L,
+    S,
+    Z,
+}
+
+pub fn tetromino_bitmask(tetromino: &Tetromino) -> u16 {
+    match tetromino {
+        Tetromino::I => I_TETROMINO,
+        Tetromino::O => O_TETROMINO,
+        Tetromino::T => T_TETROMINO,
+        Tetromino::J => J_TETROMINO,
+        Tetromino::L => L_TETROMINO,
+        Tetromino::S => S_TETROMINO,
+        Tetromino::Z => Z_TETROMINO,
+    }
+}
+
+pub fn rotate_90_clockwise(mask: u16) -> u16 {
+    let mut new = 0u16;
+
+    for y in 0..4 {
+        for x in 0..4 {
+            let bit = 1 << (15 - (y * 4 + x));
+            if mask & bit != 0 {
+                // CW rotation
+                let new_x = 3 - y;
+                let new_y = x;
+                new |= 1 << (15 - (new_y * 4 + new_x));
+            }
+        }
+    }
+
+    new
+}
+
+pub fn rotate_90_counter_clockwise(mask: u16) -> u16 {
+    let mut new = 0u16;
+
+    for y in 0..4 {
+        for x in 0..4 {
+            let bit = 1 << (15 - (y * 4 + x));
+            if mask & bit != 0 {
+                // CW: (x, y) -> (y, 3 - x)
+                new |= 1 << (15 - (y + (3 - x) * 4));
+            }
+        }
+    }
+
+    new
+}
