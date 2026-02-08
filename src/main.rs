7378e0939037728c60c46c7bb5762c60deedb1ee
Amy Opas <amy.opas@proton.me>
2026-02-09 00:44:04 +0100

    Add all tetromino permutation bitmasks

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
 mod tetromino;
 
 use germterm::{
-    color::{Color, lerp},
+    color::Color,
     crossterm::{
-        self,
         event::{
             Event, KeyCode, KeyEvent, KeyEventKind, KeyboardEnhancementFlags,
-            PopKeyboardEnhancementFlags, PushKeyboardEnhancementFlags,
+            PushKeyboardEnhancementFlags,
         },
         execute,
     },
-    draw::{Layer, draw_rect, draw_twoxel},
+    draw::{Layer, draw_twoxel},
     engine::{Engine, end_frame, exit_cleanup, init, start_frame},
     input::poll_input,
 };
 use std::io;
 
 use crate::tetromino::{
-    Tetromino, rotate_90_clockwise, rotate_90_counter_clockwise, scale_lightness, tetromino_bitmask,
+    Rotation, Tetromino, rotate_clockwise, rotate_counter_clockwise, tetromino_bitmask,
 };
 
 struct TetrominoState {
-    bitmask: u16,
+    tetromino: Tetromino,
+    rotation: Rotation,
+    color: Color,
     x: i16,
     y: i16,
-    color: Color,
 }
 
 fn main() -> io::Result<()> {
@@ -32,14 +32,19 @@ fn main() -> io::Result<()> {
     let mut layer = Layer::new(&mut engine, 0);
 
     init(&mut engine)?;
+    execute!(
+        engine.stdout,
+        PushKeyboardEnhancementFlags(KeyboardEnhancementFlags::REPORT_ALL_KEYS_AS_ESCAPE_CODES),
+    )?;
 
     let fall_speed: f32 = 1.0;
     let mut fall_timer: f32 = 0.0;
     let mut controlled_tetromino = TetrominoState {
-        bitmask: tetromino_bitmask(&Tetromino::T),
+        tetromino: Tetromino::T,
+        rotation: Rotation::North,
         x: 4,
         y: 4,
-        color: Color::new(255, 0, 255, 255),
+        color: Color::ORANGE,
     };
 
     'update_loop: loop {
@@ -54,15 +59,15 @@ fn main() -> io::Result<()> {
                     kind: KeyEventKind::Press,
                     ..
                 }) => {
-                    controlled_tetromino.bitmask =
-                        rotate_90_counter_clockwise(controlled_tetromino.bitmask)
+                    controlled_tetromino.rotation =
+                        rotate_counter_clockwise(controlled_tetromino.rotation)
                 }
                 Event::Key(KeyEvent {
                     code: KeyCode::Char('r'),
                     kind: KeyEventKind::Press,
                     ..
                 }) => {
-                    controlled_tetromino.bitmask = rotate_90_clockwise(controlled_tetromino.bitmask)
+                    controlled_tetromino.rotation = rotate_clockwise(controlled_tetromino.rotation)
                 }
                 Event::Key(KeyEvent {
                     code: KeyCode::Left,
@@ -88,16 +93,6 @@ fn main() -> io::Result<()> {
 
         draw_tetromino(&mut layer, &controlled_tetromino);
 
-        // draw_block(&mut layer, 8, 6);
-        // draw_block(&mut layer, 9, 6);
-        // draw_block(&mut layer, 9, 5);
-        // draw_block(&mut layer, 10, 6);
-
-        // draw_rect(&mut layer, 10, 10, 8, 1, Color::RED);
-        // fill_screen(&mut layer, Color::BLACK);
-        // draw_text(&mut layer, 14, 9, "Hello world!");
-        // draw_fps_counter(&mut layer, 0, 0);
-
         end_frame(&mut engine)?;
     }
 
@@ -108,13 +103,15 @@ fn main() -> io::Result<()> {
 
 /// Coordinate space: 2x2 twoxel grid
 fn draw_tetromino(layer: &mut Layer, tetromino_state: &TetrominoState) {
+    let bitmask = tetromino_bitmask(tetromino_state.tetromino, tetromino_state.rotation);
+
     for tetromino_y in 0..4 {
         for tetromino_x in 0..4 {
             let pixel_x = tetromino_state.x + tetromino_x;
             let pixel_y = tetromino_state.y + tetromino_y;
 
             let bit = 1 << (15 - (tetromino_y * 4 + tetromino_x));
-            if tetromino_state.bitmask & bit != 0 {
+            if bitmask & bit != 0 {
                 draw_block(layer, pixel_x, pixel_y, tetromino_state.color)
             }
         }
@@ -125,9 +122,9 @@ fn draw_tetromino(layer: &mut Layer, tetromino_state: &TetrominoState) {
 fn draw_block(layer: &mut Layer, x: i16, y: i16, color: Color) {
     let offsets = [
         (0, 0, color),
-        (1, 0, scale_lightness(color, 0.9)),
-        (0, 1, scale_lightness(color, 0.9)),
-        (1, 1, scale_lightness(color, 0.7)),
+        (1, 0, scale_lightness(color, 0.8)),
+        (0, 1, scale_lightness(color, 0.8)),
+        (1, 1, scale_lightness(color, 0.6)),
     ];
 
     let base_x = x as f32 * 2.0;
@@ -136,4 +133,15 @@ fn draw_block(layer: &mut Layer, x: i16, y: i16, color: Color) {
     for (dx, dy, color) in offsets {
         draw_twoxel(layer, base_x + dx as f32, base_y + dy as f32 * 0.5, color);
     }
+}
+
+pub fn scale_lightness(c: Color, factor: f32) -> Color {
+    let factor_q8 = (factor * 256.0).round() as i32;
+    let factor_q8 = factor_q8.clamp(0, 511) as u16;
+
+    let r = ((c.r() as u16).saturating_mul(factor_q8) >> 8).min(255) as u8;
+    let g = ((c.g() as u16).saturating_mul(factor_q8) >> 8).min(255) as u8;
+    let b = ((c.b() as u16).saturating_mul(factor_q8) >> 8).min(255) as u8;
+
+    Color::new(r, g, b, c.a())
 }
--- src/tetromino.rs
+++ src/tetromino.rs
@@ -1,139 +1,230 @@
-use germterm::color::Color;
+const fn build_tetromino_bitmasks(
+    north: [&str; 4],
+    east: [&str; 4],
+    south: [&str; 4],
+    west: [&str; 4],
+) -> [u16; 4] {
+    let mut rotation_index = 0;
+    let all_rotations: [[&str; 4]; 4] = [north, east, south, west];
+    let mut all_rotations_bitmasks: [u16; 4] = [0, 0, 0, 0];
 
-const fn build_tetromino_bitmask(rows: [&str; 4]) -> u16 {
-    let mut bits = 0u16;
-    let mut y = 0;
-    while y < 4 {
-        let bytes = rows[y].as_bytes();
-        let mut x = 0;
-        while x < 4 {
-            if bytes[x] == b'X' {
-                bits |= 1 << (15 - (y * 4 + x));
+    while rotation_index < 4 {
+        let mut row = 0;
+
+        while row < 4 {
+            let bytes = all_rotations[rotation_index][row].as_bytes();
+            let mut x = 0;
+            while x < 4 {
+                if bytes[x] == b'x' {
+                    all_rotations_bitmasks[rotation_index] |= 1 << (15 - (row * 4 + x));
+                }
+                x += 1;
             }
-            x += 1;
+            row += 1;
         }
-        y += 1;
+        rotation_index += 1
     }
-    bits
+    all_rotations_bitmasks
 }
 
 #[rustfmt::skip]
-pub mod tetromino_definitions {
-    use crate::tetromino::build_tetromino_bitmask;
+mod tetromino_bitmask_definitions {
+    use crate::tetromino::build_tetromino_bitmasks;
 
-    pub const I_NORTH: u16 = build_tetromino_bitmask([
-        "----",
-        "XXXX",
-        "----",
-        "----",
-    ]);
+    pub const I: [u16; 4] = build_tetromino_bitmasks(
+        [
+            "----",
+            "xxxx",
+            "----",
+            "----",
+        ],
+        [
+            "--x-",
+            "--x-",
+            "--x-",
+            "--x-",
+        ],
+        [
+            "----",
+            "----",
+            "xxxx",
+            "----",
+        ],
+        [
+            "-x--",
+            "-x--",
+            "-x--",
+            "-x--",
+        ],
+    );
 
-    pub const I_EAST: u16 = build_tetromino_bitmask([
-        "--X-",
-        "--X-",
-        "--X-",
-        "--X-",
-    ]);
+    pub const J: [u16; 4] = build_tetromino_bitmasks(
+        [
+            "x---",
+            "xxx-",
+            "----",
+            "----",
+        ],
+        [
+            "-xx-",
+            "-x--",
+            "-x--",
+            "----",
+        ],
+        [
+            "----",
+            "xxx-",
+            "--x-",
+            "----",
+        ],
+        [
+            "-x--",
+            "-x--",
+            "xx--",
+            "----",
+        ],
+    );
 
-    pub const I_SOUTH: u16 = build_tetromino_bitmask([
-        "----",
-        "----",
-        "XXXX",
-        "----",
-    ]);
+    pub const L: [u16; 4] = build_tetromino_bitmasks(
+        [
+            "--x-",
+            "xxx-",
+            "----",
+            "----",
+        ],
+        [
+            "-x--",
+            "-x--",
+            "-xx-",
+            "----",
+        ],
+        [
+            "----",
+            "xxx-",
+            "x---",
+            "----",
+        ],
+        [
+            "xx--",
+            "-x--",
+            "-x--",
+            "----",
+        ],
+    );
 
-    pub const I_WEST: u16 = build_tetromino_bitmask([
-        "-x--",
-        "-X--",
-        "-x--",
-        "-X--",
-    ]);
 
-    pub const T_NORTH: u16 = build_tetromino_bitmask([
-        "-X--",
-        "XXX-",
-        "----",
-        "----",
-    ]);
+    pub const O: [u16; 4] = build_tetromino_bitmasks(
+        [
+            "-xx-",
+            "-xx-",
+            "----",
+            "----",
+        ],
+        [
+            "-xx-",
+            "-xx-",
+            "----",
+            "----",
+        ],
+        [
+            "-xx-",
+            "-xx-",
+            "----",
+            "----",
+        ],
+        [
+            "-xx-",
+            "-xx-",
+            "----",
+            "----",
+        ],
+    );
 
-    pub const T_EAST: u16 = build_tetromino_bitmask([
-        "-X--",
-        "-XX-",
-        "-X--",
-        "----",
-    ]);
 
-    pub const T_SOUTH: u16 = build_tetromino_bitmask([
-        "----",
-        "xxx-",
-        "-x--",
-        "----",
-    ]);
+    pub const S: [u16; 4] = build_tetromino_bitmasks(
+        [
+            "-xx-",
+            "xx--",
+            "----",
+            "----",
+        ],
+        [
+            "-x--",
+            "-xx-",
+            "--x-",
+            "----",
+        ],
+        [
+            "----",
+            "-xx-",
+            "xx--",
+            "----",
+        ],
+        [
+            "x---",
+            "xx--",
+            "-x--",
+            "----",
+        ],
+    );
 
-    pub const T_WEST: u16 = build_tetromino_bitmask([
-        "-x--",
-        "XX--",
-        "-x--",
-        "----",
-    ]);
+    pub const T: [u16; 4] = build_tetromino_bitmasks(
+        [
+            "-x--",
+            "xxx-",
+            "----",
+            "----",
+        ],
+        [
+            "-x--",
+            "-xx-",
+            "-x--",
+            "----",
+        ],
+        [
+            "----",
+            "xxx-",
+            "-x---",
+            "----",
+        ],
+        [
+            "-x--",
+            "xx--",
+            "-x--",
+            "----",
+        ],
+    );
+
+
+    pub const Z: [u16; 4] = build_tetromino_bitmasks(
+        [
+            "xx--",
+            "-xx-",
+            "----",
+            "----",
+        ],
+        [
+            "--x-",
+            "-xx-",
+            "-x--",
+            "----",
+        ],
+        [
+            "----",
+            "xx--",
+            "-xx-",
+            "----",
+        ],
+        [
+            "-x--",
+            "xx--",
+            "x---",
+            "----",
+        ],
+    );
 }
 
-#[rustfmt::skip]
-pub const I_TETROMINO: u16 = build_tetromino_bitmask([
-    "----",
-    "XXXX",
-    "----",
-    "----",
-]);
-
-#[rustfmt::skip]
-pub const O_TETROMINO: u16 = build_tetromino_bitmask([
-    "XX--",
-    "XX--",
-    "----",
-    "----",
-]);
-
-#[rustfmt::skip]
-pub const T_TETROMINO: u16 = build_tetromino_bitmask([
-    "XXX-",
-    "-X--",
-    "----",
-    "----",
-]);
-
-#[rustfmt::skip]
-pub const J_TETROMINO: u16 = build_tetromino_bitmask([
-    "-o--",
-    "-o--",
-    "oo--",
-    "----",
-]);
-
-#[rustfmt::skip]
-pub const L_TETROMINO: u16 = build_tetromino_bitmask([
-    "o---",
-    "o---",
-    "oo--",
-    "----",
-]);
-
-#[rustfmt::skip]
-pub const S_TETROMINO: u16 = build_tetromino_bitmask([
-    "-oo-",
-    "oo--",
-    "----",
-    "----",
-]);
-
-#[rustfmt::skip]
-pub const Z_TETROMINO: u16 = build_tetromino_bitmask([
-    "oo--",
-    "-oo-",
-    "----",
-    "----",
-]);
-
+#[derive(Clone, Copy)]
 pub enum Tetromino {
     I,
     O,
@@ -144,61 +235,47 @@ pub enum Tetromino {
     Z,
 }
 
-pub fn tetromino_bitmask(tetromino: &Tetromino) -> u16 {
+#[derive(Clone, Copy)]
+#[repr(usize)]
+pub enum Rotation {
+    North = 0,
+    East = 1,
+    South = 2,
+    West = 3,
+}
+
+pub fn tetromino_bitmask(tetromino: Tetromino, rotation: Rotation) -> u16 {
+    use tetromino_bitmask_definitions as bitmasks;
+
+    let rotation_index: usize = rotation as usize;
+
     match tetromino {
-        Tetromino::I => I_TETROMINO,
-        Tetromino::O => O_TETROMINO,
-        Tetromino::T => T_TETROMINO,
-        Tetromino::J => J_TETROMINO,
-        Tetromino::L => L_TETROMINO,
-        Tetromino::S => S_TETROMINO,
-        Tetromino::Z => Z_TETROMINO,
+        Tetromino::I => bitmasks::I[rotation_index],
+        Tetromino::O => bitmasks::O[rotation_index],
+        Tetromino::T => bitmasks::T[rotation_index],
+        Tetromino::J => bitmasks::J[rotation_index],
+        Tetromino::L => bitmasks::L[rotation_index],
+        Tetromino::S => bitmasks::S[rotation_index],
+        Tetromino::Z => bitmasks::Z[rotation_index],
     }
 }
 
-pub fn rotate_90_clockwise(mask: u16) -> u16 {
-    let mut new = 0u16;
-
-    for y in 0..4 {
-        for x in 0..4 {
-            let bit = 1 << (15 - (y * 4 + x));
-            if mask & bit != 0 {
-                // CW rotation
-                let new_x = 3 - y;
-                let new_y = x;
-                new |= 1 << (15 - (new_y * 4 + new_x));
-            }
-        }
+pub fn rotate_clockwise(rotation: Rotation) -> Rotation {
+    match rotation {
+        Rotation::North => Rotation::East,
+        Rotation::East => Rotation::South,
+        Rotation::South => Rotation::West,
+        Rotation::West => Rotation::North,
     }
-
-    normalize_mask(new)
 }
 
-pub fn rotate_90_counter_clockwise(mask: u16) -> u16 {
-    let mut new = 0u16;
-
-    for y in 0..4 {
-        for x in 0..4 {
-            let bit = 1 << (15 - (y * 4 + x));
-            if mask & bit != 0 {
-                // CW: (x, y) -> (y, 3 - x)
-                new |= 1 << (15 - (y + (3 - x) * 4));
-            }
-        }
+pub fn rotate_counter_clockwise(rotation: Rotation) -> Rotation {
+    match rotation {
+        Rotation::North => Rotation::West,
+        Rotation::West => Rotation::South,
+        Rotation::South => Rotation::East,
+        Rotation::East => Rotation::North,
     }
-
-    normalize_mask(new)
-}
-
-pub fn scale_lightness(c: Color, factor: f32) -> Color {
-    let factor_q8 = (factor * 256.0).round() as i32;
-    let factor_q8 = factor_q8.clamp(0, 511) as u16;
-
-    let r = ((c.r() as u16).saturating_mul(factor_q8) >> 8).min(255) as u8;
-    let g = ((c.g() as u16).saturating_mul(factor_q8) >> 8).min(255) as u8;
-    let b = ((c.b() as u16).saturating_mul(factor_q8) >> 8).min(255) as u8;
-
-    Color::new(r, g, b, c.a())
 }
 
 fn normalize_mask(mask: u16) -> u16 {
